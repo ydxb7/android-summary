b101c4c97678cc9d3176e196b99289e7dcca07e4
Yini <ydxb7@mail.missouri.edu>
2019-09-15 07:17:40 -0700

    add light sensor example

--- AsyncTask/README.md
+++ AsyncTask/README.md
@@ -1,4 +1,5 @@
-# AsyncTask
+# AsyncTask and AsyncTaskLoader
+## AsyncTask
 
 ![image](show_asyncTask.gif)
 
@@ -40,6 +41,15 @@ Once created, a task is executed very simply:
 
 **`onProgressUpdate(Progress...)`**, invoked on the UI thread after a call to `publishProgress(Progress...)`. The timing of the execution is undefined. This method is used to display any form of progress in the user interface while the background computation is still executing. For instance, it can be used to animate a progress bar or show logs in a text field.
 
-##Cancelling a task
+### Cancelling a task
 A task can be cancelled at any time by invoking `cancel(boolean)`. Invoking this method will cause subsequent calls to `isCancelled()` to return true. After invoking this method, `onCancelled(java.lang.Object)`, instead of `onPostExecute(java.lang.Object)` will be invoked after `doInBackground(java.lang.Object[])` returns. **To ensure that a task is cancelled as quickly as possible, you should always check the return value of `isCancelled()` periodically from `doInBackground(java.lang.Object[])`, if possible (inside a loop for instance.)**
 
+## AsyncTaskLoader
+
+**AsyncTask 缺点**: 正在下载的时候旋转屏幕，得到的result会传递给一个已经不存在的activity。使用component LoaderManager的Loader，他们的生命超过activity的生命周期来避免重复加载。如果我们需要在background thread load data，使用AsyncTaskLoader and prevent the reload after the user back into the App.</br>
+
+[Example](https://github.com/ydxb7/Exercises_Java/tree/master/Lesson05b-Smarter-GitHub-Repo-Search)
+
+1. **SaveResults** - 旋转设备后保存搜索结果
+2. **AsyncTaskLoader 的使用** - 把AsyncTask改成`AsyncTaskLoader`，只处理旋转的加载问题，但是如果用户离开再回来就会重新加载，用3来处理这个问题。使用 `AsyncTaskLoader` 后就不需要用 onSaveInstanceState 记录的结果
+3. **PolishAsyncTask** - 缓存加载器结果，防止客户离开应用后出现查询
---
+++ Sensor/LightSensorExample/app/src/main/java/ai/tomorrow/lightsensorexample/MainActivity.java
@@ -0,0 +1,63 @@
+package ai.tomorrow.lightsensorexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final String TAG = "MainActivity";
+
+    private SensorManager sensorManager;
+    private Sensor mLight;
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+        Log.d(TAG, "onAccuracyChanged: " + String.valueOf(accuracy));
+        mTextView.setText("Light sensor onAccuracyChanged: " + String.valueOf(accuracy));
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        float lux = event.values[0];
+        // Do something with this sensor value.
+        Log.d(TAG, "onSensorChanged: " + String.valueOf(lux));
+        mTextView.setText("Light sensor onSensorChanged: " + String.valueOf(lux));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
+
